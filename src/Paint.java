1797eacb21530708d704057082eb57d0312029aa
TanatP1 <TanatP1@bitbucket.org>
2023-06-25 16:43:42 +0700

    650615019 Strawberry moon picture

---
+++ src/Paint.java
@@ -0,0 +1,200 @@
+public class Paint {
+    public static void main(String[] args) {
+        Turtle bob = new Turtle();
+        //Drawing strawberry moon
+        bob.up();
+        bob.down(); 
+        bob.penColor("red");
+        bob.forward(100);
+        bob.left(35);
+        bob.forward(50);
+        bob.left(25);
+        bob.forward(50);
+        bob.left(30);
+        bob.forward(50);
+        bob.left(25);
+        bob.forward(50);
+        bob.left(35);
+        bob.forward(50);
+        bob.left(30);
+        bob.forward(90);
+        bob.left(25);
+        bob.forward(50);
+        bob.left(35);
+        bob.forward(50);
+        bob.left(30);
+        bob.forward(50);
+        bob.left(25);
+        bob.forward(50);
+        bob.left(20);
+        bob.forward(50);
+
+        //1st star
+        bob.up();
+        bob.left(30);
+        bob.forward(150);
+        bob.down();
+        bob.penColor("orange");
+
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+
+        //2nd star
+        bob.up();
+        bob.left(60);
+        bob.forward(100);
+        bob.down();
+        bob.penColor("orange");
+
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+
+        //3rd star
+        bob.up();
+        bob.left(70);
+        bob.forward(250);
+        bob.down();
+        bob.penColor("orange");
+
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+
+        //4th star
+        bob.up();
+        bob.left(110);
+        bob.forward(400);
+        bob.down();
+        bob.penColor("orange");
+
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+
+        //5th star
+        bob.up();
+        bob.left(200);
+        bob.forward(100);
+        bob.down();
+        bob.penColor("orange");
+
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+
+        //6th star
+        bob.up();
+        bob.left(25);
+        bob.forward(100);
+        bob.down();
+        bob.penColor("orange");
+
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+        bob.left(72.25);
+        bob.forward(10);
+        bob.right(144.25);
+        bob.forward(10);
+}
+}
